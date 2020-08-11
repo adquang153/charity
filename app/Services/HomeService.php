aee8b8fcb7fd9684dd2342221d71da2dac2a362c
adquang153 <lhiu80@gmail.com>
2020-08-11 15:18:14 +0700

    Build Project

---
+++ app/Services/HomeService.php
@@ -0,0 +1,17 @@
+<?php 
+
+namespace App\Services;
+
+use DB;
+use Illuminate\Support\Facades\Password;
+use Auth;
+
+class HomeService{
+
+    public function test(){
+        return 'zxczxc';
+    }
+
+}
+
+?>
